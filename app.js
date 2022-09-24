cf4b21fd919d1a831105655870be3922e7d03d71
Viktoriia Melnyk <mviktoria1005@gmail.com>
2022-09-24 20:43:46 +0300

    start

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
 const logger = require("morgan");
 const cors = require("cors");
 require("dotenv").config();
@@ -14,6 +16,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/users", usersRouter);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });

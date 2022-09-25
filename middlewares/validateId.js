a6830eec9d3a42748522c9c16e99c822ac4611a8
Anna Kulibabina <anna.kulibabina@gmail.com>
2022-09-25 12:45:47 +0300

    removed comments

--- middlewares/validateId.js
+++ middlewares/validateId.js
@@ -2,8 +2,8 @@ const { isValidObjectId } = require("mongoose");
 const { NotFound } = require("http-errors");
 
 const validateId = (req, res, next) => {
-  const { id } = req.params; // or _id
-  const isValid = isValidObjectId(id); // or _id
+  const { id } = req.params;
+  const isValid = isValidObjectId(id);
   if (!isValid) {
     next(NotFound(`User with id (${id}) does not exist`));
     return;

a611443a6ea84ffcfbc3c5c3a3e54aeeae46e29b
Anna Miklukha <miklukha.anna@ukr.net>
2022-09-26 15:16:13 +0200

    changes book's controllers

--- models/index.js
+++ models/index.js
@@ -1,9 +1,10 @@
-const { User, joiUserSchema, joiLoginSchema } = require("./user");
-const { Book } = require("./book");
+const { User, joiUserSchema, joiLoginSchema } = require('./user');
+const { Book, joiBookSchema } = require('./book');
 
 module.exports = {
   User,
   Book,
   joiUserSchema,
   joiLoginSchema,
+  joiBookSchema,
 };

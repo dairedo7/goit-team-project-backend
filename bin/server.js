896d394599b6a28c724cb2bb04af3f8cc6e081c9
Vladyslav Svitana <vladik719@gmail.com>
2022-09-29 01:37:08 +0200

    Update server.js

--- bin/server.js
+++ bin/server.js
@@ -3,7 +3,9 @@ require("dotenv").config();
 const path = require("path");
 const app = require("../app");
 
-const { DB_HOST, PORT } = process.env;
+const { DB_HOST } = process.env;
+
+const port = process.env.PORT || 8000;
 
 mongoose
   .connect(DB_HOST, {
@@ -11,8 +13,8 @@ mongoose
     useUnifiedTopology: true,
   })
   .then(() => {
-    console.log(PORT)
-    app.listen(PORT || 3000);
+    console.log(port)
+    app.listen(port || 3000);
     app.use("/link", (req, res) => {
       res.sendFile(path.join(__dirname, "../../public/link.html"));
     });

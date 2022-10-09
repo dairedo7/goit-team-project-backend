33b97120a8221a3a61047b5bd932b8ded1934664
dairedo7 <vladik719@gmail.com>
2022-10-09 08:29:28 +0200

    updateBook-endpoint-fix

--- routes/api/books.js
+++ routes/api/books.js
@@ -1,25 +1,20 @@
-const express = require("express");
+const express = require('express');
 
-const ctrl = require("../../controllers/books");
-const { getBookStatus } = require("../../controllers");
-const { validation, validateId, auth, wrapper } = require("../../middlewares");
-const { joiBookSchema, joiBookUpdateSchema } = require("../../models");
+const ctrl = require('../../controllers/books');
+const { getBookStatus } = require('../../controllers');
+const { validation, validateId, auth, wrapper } = require('../../middlewares');
+const { joiBookSchema, joiBookUpdateSchema } = require('../../models');
 
 const router = express.Router();
 
-router.get("/", auth, wrapper(ctrl.getBooks));
+router.get('/', auth, wrapper(ctrl.getBooks));
 
-router.post("/", auth, validation(joiBookSchema), wrapper(ctrl.addBook));
+router.post('/', auth, validation(joiBookSchema), wrapper(ctrl.addBook));
 
-router.get("/get-status/:status", auth, wrapper(getBookStatus));
+router.get('/get-status/:status', auth, wrapper(getBookStatus));
 
-router.put(
-  "/:bookId",
-  auth,
-  validation(joiBookUpdateSchema),
-  wrapper(ctrl.updateBook)
-);
+router.put('/', auth, validation(joiBookUpdateSchema), wrapper(ctrl.updateBook));
 
-router.delete("/:bookId", auth, validateId, wrapper(ctrl.removeBook));
+router.delete('/:bookId', auth, validateId, wrapper(ctrl.removeBook));
 
 module.exports = router;

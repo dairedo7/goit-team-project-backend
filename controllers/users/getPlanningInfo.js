d27cc5359d9419d4ebcf9f0f6856339f01f9773f
dairedo7 <vladik719@gmail.com>
2022-12-14 17:16:54 +0100

    getPlanningInfo_changes

--- controllers/users/getPlanningInfo.js
+++ controllers/users/getPlanningInfo.js
@@ -1,30 +1,38 @@
 const { planningServices } = require('../../services');
+const {
+  httpCode: { NOT_FOUND },
+} = require('../../helpers/constants');
 
 const getPlanningInfo = async (req, res) => {
   const user = req.user;
+  try {
+    const planningBooks = await planningServices.getPlanningBooks(user.planning);
 
-  const planningBooks = await planningServices.getPlanningBooks(user.planning);
+    const books = await planningServices.getActiveBooks(user.planning);
 
-  const books = await planningServices.getActiveBooks(user.planning);
+    const planning = await planningServices.getPlanning(user.planning);
 
-  const planning = await planningServices.getPlanning(user.planning);
-
-  if (!planningBooks || !planning) {
-    const error = new Error({ message: 'Not found' });
-    error.status = 404;
+    if (!planningBooks || !planning) {
+      const error = new Error({ message: 'Not found' });
+      error.status = 404;
+      throw error;
+    }
+    res.json({
+      status: 'success',
+      code: 200,
+      planning,
+      data: {
+        books: books,
+        booksNumber: planningBooks.length,
+        planningDur: planning.duration,
+      },
+    });
+  } catch (error) {
+    // eslint-disable-next-line no-ex-assign
+    error = new Error(`${NOT_FOUND}: No active planning found`);
+    error.status = NOT_FOUND;
     throw error;
   }
-
-  res.json({
-    status: 'success',
-    code: 200,
-    planning,
-    data: {
-      books: books,
-      booksNumber: planningBooks.length,
-      planningDur: planning.duration,
-    },
-  });
 };
 
 module.exports = getPlanningInfo;

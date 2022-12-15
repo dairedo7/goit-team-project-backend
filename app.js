18deb4d8c99ffed4f96cffa6f7c361de2db2f9f3
dairedo7 <vladik719@gmail.com>
2022-12-15 19:10:38 +0100

    app_api

--- app.js
+++ app.js
@@ -1,29 +1,29 @@
-const express = require("express");
-const swaggerUi = require("swagger-ui-express");
-const swaggerDocument = require("./swagger.json");
-const logger = require("morgan");
-const cors = require("cors");
-require("dotenv").config();
+const express = require('express');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+const logger = require('morgan');
+const cors = require('cors');
+require('dotenv').config();
 
-const apiAuth = require("./routes/api/auth");
-const apiUsers = require("./routes/api/users");
-const apiBooks = require("./routes/api/books");
+const apiAuth = require('./routes/api/auth');
+const apiUsers = require('./routes/api/users');
+const apiBooks = require('./routes/api/books');
 
 const app = express();
 
-const formatsLogger = app.get("env") === "development" ? "dev" : "short";
+const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
 
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 
-app.use("/auth", apiAuth);
-app.use("/user", apiUsers);
-app.use("/book", apiBooks);
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use('/api/auth', apiAuth);
+app.use('/api/user', apiUsers);
+app.use('/book', apiBooks);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use((req, res) => {
-  res.status(404).json({ message: "Not found" });
+  res.status(404).json({ message: 'Not found' });
 });
 
 app.use((err, req, res, next) => {

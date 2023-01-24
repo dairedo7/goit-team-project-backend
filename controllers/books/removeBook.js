5aa36f68bcc6197d726af9cc82fae5642f28d651
dairedo7 <vladik719@gmail.com>
2023-01-24 20:37:07 +0100

    minor_fixes

--- controllers/books/removeBook.js
+++ controllers/books/removeBook.js
@@ -2,14 +2,14 @@ const { bookServices } = require('../../services');
 const { requestError } = require('../../helpers/requestError');
 
 const removeBook = async (req, res) => {
-  const { bookId: id } = req.params;
+  const { bookId: _id } = req.params;
   const theUser = req.user;
 
-  const result = await bookServices.removeBook(id);
+  const result = await bookServices.removeBook(_id);
   if (!result) {
     throw requestError(404, 'Not found');
   }
-  theUser?.books.splice(id, 1);
+  theUser?.books.splice(_id, 1);
   await theUser?.save();
 
   res.json({
--- middlewares/validateId.js
+++ middlewares/validateId.js
@@ -2,10 +2,10 @@ const { isValidObjectId } = require('mongoose');
 const { NotFound } = require('http-errors');
 
 const validateId = (req, res, next) => {
-  const { bookId: id } = req.params;
-  const isValid = isValidObjectId(id);
+  const { bookId } = req.params;
+  const isValid = isValidObjectId(bookId);
   if (!isValid) {
-    next(NotFound(`User with id (${id}) does not exist`));
+    next(NotFound(`Book with id (${bookId}) does not exist`));
     return;
   }
   next();
